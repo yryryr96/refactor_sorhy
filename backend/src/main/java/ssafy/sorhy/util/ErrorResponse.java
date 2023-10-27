6b4f6488df64a23717611e006816f95dafb7d58e
정영록 <s9285s@naver.com>
2023-10-27 17:49:57 +0900

    Feat : 예외 처리 클래스 설정

---
+++ backend/src/main/java/ssafy/sorhy/util/ErrorResponse.java
@@ -0,0 +1,17 @@
+package ssafy.sorhy.util;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    private final int statusCode;
+    private final String message;
+    private final String detail;
+}
