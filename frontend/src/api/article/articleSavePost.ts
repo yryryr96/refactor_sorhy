32923037f7dd22d7e9f6d5221ab6c8ce44bb67a5
전대현 <wjseogus523@naver.com>
2023-10-29 01:31:48 +0900

    Feat : user,article api 작성

--- frontend/src/api/article/articleSavePost.ts
+++ frontend/src/api/article/articleSavePost.ts
@@ -1,1 +1,22 @@
 //article 작성 Post
+
+import api from "../api";
+
+const articleSavePost = async (articleDatas : any) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    const response = await api.post("/article", articleDatas,{
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
+   
+
+    return response.data.response;
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+};
+
+export default articleSavePost;
