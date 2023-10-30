3c52aedf20e43959f793655494a0eac94ee2b1ae
정영록 <s9285s@naver.com>
2023-10-30 22:28:18 +0900

    Feat : 토큰 정보에서 해당 권한 없으면 에러 처리

---
+++ backend/src/main/java/ssafy/sorhy/exception/NotValidUserException.java
@@ -0,0 +1,8 @@
+package ssafy.sorhy.exception;
+
+public class NotValidUserException extends RuntimeException{
+
+    public NotValidUserException(String message) {
+        super(message);
+    }
+}
