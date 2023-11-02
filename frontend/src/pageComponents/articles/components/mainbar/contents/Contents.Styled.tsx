0ed431bcba507c0664270e91ca95cb80b2614068
전대현 <wjseogus523@naver.com>
2023-11-02 17:18:37 +0900

    Feat : 게시판 Contents Box 선정

--- frontend/src/pageComponents/articles/components/mainbar/contents/Contents.Styled.tsx
+++ frontend/src/pageComponents/articles/components/mainbar/contents/Contents.Styled.tsx
@@ -8,7 +8,8 @@ const StyledContentsBox = styled.div`
     padding: 2% 2%;
     border: 1px solid lightgray;
     border-radius: 20px;
-    box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em,
+        rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
     gap: 5%;
     overflow: auto;
     /* &::-webkit-scrollbar {
