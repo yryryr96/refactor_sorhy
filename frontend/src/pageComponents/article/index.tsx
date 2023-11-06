b9d7e71e377c4b6c8e24525c21b02aad5ab527eb
전대현 <wjseogus523@naver.com>
2023-11-06 11:04:23 +0900

    Feat : Article 글 작성 테스트

--- frontend/src/pageComponents/article/index.tsx
+++ frontend/src/pageComponents/article/index.tsx
@@ -2,35 +2,54 @@
 
 import { useEffect, useState } from 'react';
 import articleDetailGet from '@/api/article/articleDetailGet';
+import {
+    StyledArticle,
+    ArticleContainer,
+    StyledArticleContent,
+    StyledArticleHeader,
+    StyledArticleBody,
+} from './Article.Styled';
 import Image from 'next/image';
+import Button from '@/components/button';
+import Input from '@/components/input';
 
 export interface ArticleDetail {
-    articleId ?: number;
-    nickname ?: string;
-    title ?: string;
-    imageUrl ?: any;
-    createdAt ?: any;
+    articleId?: number;
+    nickname?: string;
+    title?: string;
+    imageUrl?: any;
+    createdAt?: any;
 }
 
 const Article = (props: any) => {
     const { articleId } = props;
-    const [articleDetail, setArticleDetail] = useState<ArticleDetail[]>([])
+    const [articleDetail, setArticleDetail] = useState<ArticleDetail[]>([]);
     useEffect(() => {
         articleDetailGet(articleId)
-            .then(res => {
+            .then((res) => {
                 setArticleDetail(res.result);
             })
-            .catch(error => {
-                console.error("에러 발생:", error);
+            .catch((error) => {
+                console.error('에러 발생:', error);
             });
     }, [articleId]);
-    console.log(articleDetail)
+    console.log(articleDetail);
     return (
-        <>
-            {articleDetail ? <div>{articleDetail.articleId} | {articleDetail.title}</div>
-            : <div>Loading...</div>}
-        </>
-        )
-    }
+        <ArticleContainer>
+            <StyledArticle>
+                <StyledArticleContent>
+                    {articleDetail ? (
+                        <>
+                            <StyledArticleHeader>z</StyledArticleHeader>
+                            <StyledArticleBody></StyledArticleBody>
+                        </>
+                    ) : (
+                        <div>Loading...</div>
+                    )}
+                </StyledArticleContent>
+            </StyledArticle>
+        </ArticleContainer>
+    );
+};
 
 export default Article;
