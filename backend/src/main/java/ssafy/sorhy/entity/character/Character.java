c074a5a90c02ccb9dc1fbdd8d18bfeaf295cf0a8
정영록 <s9285s@naver.com>
2023-11-08 12:43:46 +0900

    Feat : 캐릭터 테이블 추가 및 모스트 캐릭터 조회 api 개발

---
+++ backend/src/main/java/ssafy/sorhy/entity/character/Character.java
@@ -0,0 +1,23 @@
+package ssafy.sorhy.entity.character;
+
+import lombok.Getter;
+import ssafy.sorhy.entity.user.User;
+import ssafy.sorhy.entity.usercharacter.UserCharacter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "Characters")
+public class Character {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "character_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "character")
+    private List<UserCharacter> userCharacter;
+}
