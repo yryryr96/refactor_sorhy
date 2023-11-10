973acae575d6c5ddf7c6eb8746f81ad7670ff174
전대현 <wjseogus523@naver.com>
2023-11-10 17:45:31 +0900

    Feat : 전적 검색 Right 기본 완료

--- frontend/src/pageComponents/recordsearch/index.tsx
+++ frontend/src/pageComponents/recordsearch/index.tsx
@@ -10,18 +10,20 @@ const RecordSearch = (props: any) => {
     const { userId } = props;
 
     return (
-        <StyledRecordMain>
-            <StyledRecordFrame>
-                <StyledLeftContainer>
-                    <LeftTop userId={userId} />
-                    <LeftMid userId={userId} />
-                    <LeftBottom userId={userId} />
-                </StyledLeftContainer>
-                <StyledRightContainer>
-                    <Right userId={userId} />
-                </StyledRightContainer>
-            </StyledRecordFrame>
-        </StyledRecordMain>
+        <>
+            <StyledRecordMain>
+                <StyledRecordFrame>
+                    <StyledLeftContainer>
+                        <LeftTop userId={userId} />
+                        <LeftMid userId={userId} />
+                        <LeftBottom userId={userId} />
+                    </StyledLeftContainer>
+                    <StyledRightContainer>
+                        <Right userId={userId} />
+                    </StyledRightContainer>
+                </StyledRecordFrame>
+            </StyledRecordMain>
+        </>
     );
 };
 
