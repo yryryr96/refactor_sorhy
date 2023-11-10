b2eb7527926cbe963a7e463e2c36a4cb024fd227
전대현 <wjseogus523@naver.com>
2023-11-10 14:29:11 +0900

    Feat : 전적 검색 Right 초상화 완료

--- frontend/src/pageComponents/recordsearch/components/right/Right.Styled.tsx
+++ frontend/src/pageComponents/recordsearch/components/right/Right.Styled.tsx
@@ -3,12 +3,12 @@ import styled, { css } from 'styled-components';
 const StyledRightHeader = styled.div`
     display: flex;
     flex-direction: column;
-    padding: 4% 0;
+    padding: 5% 0;
     width: 100%;
     height: 23.5%;
     font-size: 30px;
     font-weight: bold;
-    border: 1px solid lightgray;
+    /* border: 1px solid lightgray; */
     color: #86bebe;
 `;
 
@@ -16,7 +16,7 @@ const StyledRightBody = styled.div`
     display: flex;
     flex-direction: column;
     padding: 3% 3%;
-    width: 100%;
+    width: 90%;
     height: 71.5%;
     border: 1px solid gray;
     border-radius: 20px;
@@ -25,10 +25,77 @@ const StyledRightBody = styled.div`
 
 const StyledRecord = styled.div`
     display: flex;
+    flex-direction: row;
     width: 100%;
     height: 20%;
     border: 1px solid #bfbfbf;
     border-radius: 20px;
 `;
 
-export { StyledRecord, StyledRightHeader, StyledRightBody };
+const StyledRecordColor = styled.div<any>`
+    display: flex;
+    width: 4%;
+    background-color: ${(props) => props.background || '#218fff'};
+
+    border-bottom-left-radius: 20px;
+    border-top-left-radius: 20px;
+`;
+
+const StyledRecordMain = styled.div<any>`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    height: 100%;
+    border: 1px solid black;
+`;
+
+const StyledRecordMainTop = styled.div<any>`
+    display: flex;
+    width: 100%;
+    height: 30%;
+    border: 1px solid black;
+`;
+const StyledRecordMainBottom = styled.div<any>`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 70%;
+    border: 1px solid black;
+`;
+
+const StyledRecordBottomLeft = styled.div<any>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40%;
+    height: 100%;
+    border: 1px solid black;
+`;
+
+const StyledRecordBottomRight = styled.div<any>`
+    display: flex;
+    align-items: center;
+    width: 60%;
+    height: 100%;
+    border: 1px solid black;
+`;
+
+const StyledRecordVS = styled.div<any>`
+    display: flex;
+    width: 46%;
+    height: 100%;
+    border: 1px solid black;
+`;
+
+export {
+    StyledRecordBottomLeft,
+    StyledRecordBottomRight,
+    StyledRecordMainBottom,
+    StyledRecordMainTop,
+    StyledRecordVS,
+    StyledRecordMain,
+    StyledRecordColor,
+    StyledRecord,
+    StyledRightHeader,
+    StyledRightBody,
+};
--- frontend/src/pageComponents/recordsearch/components/right/index.tsx
+++ frontend/src/pageComponents/recordsearch/components/right/index.tsx
@@ -1,6 +1,18 @@
 'use client';
 
-import { StyledRecord, StyledRightHeader, StyledRightBody } from './Right.Styled';
+import {
+    StyledRecordBottomLeft,
+    StyledRecordBottomRight,
+    StyledRecordMainBottom,
+    StyledRecordMainTop,
+    StyledRecordVS,
+    StyledRecordMain,
+    StyledRecordColor,
+    StyledRecord,
+    StyledRightHeader,
+    StyledRightBody,
+} from './Right.Styled';
+import Image from 'next/image';
 
 const Right = (props: any) => {
     const { userId } = props;
@@ -11,11 +23,37 @@ const Right = (props: any) => {
                 <div>전적 히스토리</div>
             </StyledRightHeader>
             <StyledRightBody>
-                <StyledRecord />
-                <StyledRecord />
-                <StyledRecord />
-                <StyledRecord />
-                <StyledRecord />
+                <StyledRecord>
+                    <StyledRecordColor background="red" />
+                    <StyledRecordMain>
+                        <StyledRecordMainTop>z</StyledRecordMainTop>
+                        <StyledRecordMainBottom>
+                            <StyledRecordBottomLeft>
+                                <Image
+                                    src="/chr1.png"
+                                    width={65}
+                                    height={65}
+                                    alt="게임 내 초상화"
+                                    style={{ borderRadius: '30px' }}
+                                />
+                            </StyledRecordBottomLeft>
+                            <StyledRecordBottomRight></StyledRecordBottomRight>
+                        </StyledRecordMainBottom>
+                    </StyledRecordMain>
+                    <StyledRecordVS></StyledRecordVS>
+                </StyledRecord>
+                <StyledRecord>
+                    <StyledRecordColor background="#218fff" />
+                </StyledRecord>
+                <StyledRecord>
+                    <StyledRecordColor background="red" />
+                </StyledRecord>
+                <StyledRecord>
+                    <StyledRecordColor background="#218fff" />
+                </StyledRecord>
+                <StyledRecord>
+                    <StyledRecordColor background="red" />
+                </StyledRecord>
             </StyledRightBody>
         </>
     );
