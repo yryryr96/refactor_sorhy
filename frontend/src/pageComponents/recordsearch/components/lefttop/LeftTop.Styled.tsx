37ccff837539905370abdd56a428bc503f4f5543
전대현 <wjseogus523@naver.com>
2023-11-10 10:05:58 +0900

    Feat : 전적 검색 LeftTop 작업중

--- frontend/src/pageComponents/recordsearch/components/lefttop/LeftTop.Styled.tsx
+++ frontend/src/pageComponents/recordsearch/components/lefttop/LeftTop.Styled.tsx
@@ -3,9 +3,76 @@ import styled, { css } from 'styled-components';
 const StyledLeftTopContainer = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
     width: 100%;
     height: 30%;
     border: 1px solid black;
 `;
 
-export { StyledLeftTopContainer };
+const StyledTopContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+`;
+
+const StyledBottomContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 60%;
+    height: 100%;
+    border: 1px solid red;
+    margin-left: 40%;
+`;
+
+const StyledPictureSide = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40%;
+    height: 100%;
+`;
+
+const StyledTitleInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 60%;
+    height: 100%;
+`;
+
+const StyledTopTitle = styled.div`
+    display: flex;
+
+    align-items: center;
+    width: 100%;
+    height: 60%;
+
+    font-size: 33px;
+    font-weight: bold;
+`;
+
+const StyledBottomTitle = styled.div`
+    display: flex;
+    width: 100%;
+    height: 40%;
+    font-size: 19px;
+    color: gray;
+`;
+
+const StyledUpdateRecord = styled.div`
+    display: flex;
+    width: 40%;
+    height: 40%;
+    border: 1px solid black;
+`;
+
+export {
+    StyledUpdateRecord,
+    StyledTopTitle,
+    StyledBottomTitle,
+    StyledBottomContainer,
+    StyledTopContainer,
+    StyledLeftTopContainer,
+    StyledPictureSide,
+    StyledTitleInfo,
+};
--- frontend/src/pageComponents/recordsearch/components/lefttop/index.tsx
+++ frontend/src/pageComponents/recordsearch/components/lefttop/index.tsx
@@ -1,14 +1,50 @@
 'use client';
 
-import { StyledLeftTopContainer } from './LeftTop.Styled';
+import { useState, useEffect } from 'react';
+import {
+    StyledUpdateRecord,
+    StyledTopTitle,
+    StyledBottomTitle,
+    StyledBottomContainer,
+    StyledTopContainer,
+    StyledLeftTopContainer,
+    StyledPictureSide,
+    StyledTitleInfo,
+} from './LeftTop.Styled';
+import Image from 'next/image';
 
 const LeftTop = (props: any) => {
     const { userId } = props;
+    const [randomNumber, setRandomNumber] = useState<number>(1);
+    const getRandomNumber = () => {
+        const newRandomNumber = Math.floor(Math.random() * 10) + 1;
+
+        setRandomNumber(newRandomNumber);
+    };
+    useEffect(() => {
+        getRandomNumber();
+    }, []);
 
     return (
         <StyledLeftTopContainer>
-            <div>good</div>
-            <div>good</div>
+            <StyledTopContainer>
+                <StyledPictureSide>
+                    <Image
+                        src={`/chr${randomNumber}.png`}
+                        width={120}
+                        height={120}
+                        alt={'캐릭터 초상화'}
+                        style={{ borderRadius: '60px' }}
+                    />
+                </StyledPictureSide>
+                <StyledTitleInfo>
+                    <StyledTopTitle>Im신뢰에요</StyledTopTitle>
+                    <StyledBottomTitle>소속 : CJ</StyledBottomTitle>
+                </StyledTitleInfo>
+            </StyledTopContainer>
+            <StyledBottomContainer>
+                <StyledUpdateRecord>good</StyledUpdateRecord>
+            </StyledBottomContainer>
         </StyledLeftTopContainer>
     );
 };
