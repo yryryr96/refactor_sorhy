6d9daecd2aa1d48b7b477a1002bfab1709a45a8f
전대현 <wjseogus523@naver.com>
2023-11-12 00:17:47 +0900

    Feat : user 검색 여결

--- frontend/src/pageComponents/recordsearch/index.tsx
+++ frontend/src/pageComponents/recordsearch/index.tsx
@@ -1,14 +1,29 @@
 'use client';
 
+import { useState,useEffect } from 'react';
 import { StyledRecordMain, StyledRecordFrame, StyledLeftContainer, StyledRightContainer } from './RecordSearch.Styled';
 import LeftBottom from './components/leftbottom';
 import LeftTop from './components/lefttop';
 import LeftMid from './components/leftmid';
 import Right from './components/right';
+import { loadBindings } from 'next/dist/build/swc';
+import userSearchGet from '@/api/search/userSearchGet';
 
 const RecordSearch = (props: any) => {
     const { userId } = props;
-
+    const [userInfo, setUserInfo] = useState<any>([]);
+    const [loading,setLoading] = useState(true);
+    useEffect(() => {
+        userSearchGet(userId)
+            .then((res) => {
+                setUserInfo(res.result);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('에러가 발생하였습니다:', error);
+            });
+    }, []);
+    console.log(userInfo)
     return (
         <>
             <StyledRecordMain>
