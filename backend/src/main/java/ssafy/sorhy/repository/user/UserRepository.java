84a4559517e3ff7ddfea6ae1dff4629be13573b5
정영록 <s9285s@naver.com>
2023-11-13 17:23:45 +0900

    feat : 유저별 각 게임 최고점수 구현

--- backend/src/main/java/ssafy/sorhy/repository/user/UserRepository.java
+++ backend/src/main/java/ssafy/sorhy/repository/user/UserRepository.java
@@ -2,9 +2,12 @@ package ssafy.sorhy.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import ssafy.sorhy.dto.user.UserDto;
+import ssafy.sorhy.dto.user.UserEachGameScore;
 import ssafy.sorhy.dto.user.UserRankInfoDto;
 import ssafy.sorhy.entity.user.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -23,4 +26,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "select new ssafy.sorhy.dto.user.UserRankInfoDto(count(u), (select count(u)+1 from User u where u.totalScore > (select u2.totalScore from User u2 where u2.nickname = :nickname))) from User u")
     UserRankInfoDto findUserRankInfo(String nickname);
+
+    @Query("select new ssafy.sorhy.dto.user.UserEachGameScore(r.gameTitle, r.score) " +
+            "from Ranking r " +
+            "join r.user u " +
+            "where u.nickname = :nickname")
+    List<UserEachGameScore> findEachGameTopScore(String nickname);
 }
